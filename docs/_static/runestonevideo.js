3126a9773575b25fa220bed29a124d4157b24ecf
Lazar Bojičić <lazar.bojicic42@gmail.com>
2019-09-16 15:37:40 +0200

    published lazout changes

--- docs/_static/runestonevideo.js
+++ docs/_static/runestonevideo.js
@@ -17,3 +17,34 @@ function onPlayerStateChange(event) {
     }
     rb.logBookEvent(data);
   }
+
+
+
+window.onload = function (){
+    var allVideos = document.getElementsByClassName("ytvideo");
+    var allVideosCloser = document.getElementsByClassName("ytvideoclose");
+
+    var toggleVideo = function (videoId){
+        var srcValue = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+        if ($('#modal').css("display") == "none") {
+            document.getElementById("YTmodal").innerHTML = "<iframe id='ytplayer' style='height: 90vh; width: 90vw;' src='" + srcValue + "' allowfullscreen></iframe>";
+            document.getElementById("modal").setAttribute("style", "position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: rgba(140, 140, 140, 0.3); z-index: 10000;");
+        } else {
+            document.getElementById("YTmodal").innerHTML = "";
+            document.getElementById("modal").setAttribute("style", "display: none; position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: rgba(140, 140, 140, 0.3); z-index: 10000;");
+        }
+    };
+    if (allVideos.length > 0)
+    Array.prototype.slice.call(allVideos).forEach (function(video) {
+        video.addEventListener('click', function() {
+            toggleVideo(video.id);
+        });
+    });
+
+    if (allVideosCloser.length > 0)
+    Array.prototype.slice.call(allVideosCloser).forEach (function(video) {
+        video.addEventListener('click', function() {
+            toggleVideo();
+        });
+    });
+}
